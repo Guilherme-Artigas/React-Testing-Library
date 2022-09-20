53c6a60b32936464c2fe946b4f9dcc8c59991ab2
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-09-20 07:35:16 -0300

    Requisito 05

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Verificando comportamento do componente NotFound', () => {
+  it('Verifica se a página contem um título infomando página não encontrada', () => {
+    renderWithRouter(<NotFound />);
+
+    const titleNotFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  it('Verifica se carrega uma imagem na página não encontrada', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
